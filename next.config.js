361653c0c8f3c9bbe495a3dd954c2f03852496eb
Phangantuk <fedor22081994@gmail.com>
2026-03-08 08:17:12 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // App Router is enabled by default in Next.js 14
-  // Add any custom config here
-}
-
-module.exports = nextConfig
